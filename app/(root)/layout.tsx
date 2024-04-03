06e280dcf29e97953c96a8c390151cb90d411789
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-04-03 18:36:10 +0100

    updated root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "../globals.css";
 import { Inter as FontSans } from "next/font/google";
-// import "@/styles/globals.css"
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar";
 import { SubNav } from "@/components/sub-nav";
@@ -16,25 +15,6 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-const metadata = {
-  // metadataBase: new URL(defaultUrl),
-  title: "VendorSpot",
-  description: "Buy Anything, Anywhere, Anytime",
-};
-
-type LayoutProps = {
-  children: React.ReactNode;
-  isDashboard?: boolean;
-  session?: any;
-};
-const revalidate = 0;
-
-// <AuthProvider accessToken={session?.access_token}>{children}</AuthProvider>;
-
 export default function Layout({
   children,
   session,
@@ -43,17 +23,19 @@ export default function Layout({
   session: any;
 }) {
   return (
-    <html lang="en" className={cn(GeistSans.className)}>
-      <body className={cn(fontSans.variable)}>
-        <main className="min-h-screen flex flex-col items-center md:p-0 ">
-          <AuthProvider accessToken={session?.access_token}>
-            <Navbar />
-            <SubNav />
-            <div className="wrapper">{children}</div>
-            <Footer />
-          </AuthProvider>
-        </main>
-      </body>
-    </html>
+    <div
+      className={cn(
+        GeistSans.className,
+        fontSans.variable,
+        "min-h-screen flex flex-col items-center md:p-0"
+      )}
+    >
+      <AuthProvider accessToken={session?.access_token}>
+        <Navbar />
+        <SubNav />
+        <div className="wrapper">{children}</div>
+        <Footer />
+      </AuthProvider>
+    </div>
   );
 }
