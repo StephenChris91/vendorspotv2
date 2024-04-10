7a14a1a23f7b0042449a10d5e9c5c3d012f6b435
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-04-10 21:25:34 +0100

    ignore case

--- app/hooks/useLogin.tsx
+++ app/hooks/useLogin.tsx
@@ -1,6 +1,6 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import Login from "@/components/auth/signin";
 
