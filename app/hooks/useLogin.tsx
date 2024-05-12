cc00a56b8b9bc7e03f31378961f95549815752a5
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-05-12 22:23:53 +0100

    extended user session to include extra information

--- app/hooks/useLogin.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-import Login from "@/components/auth/signin";
-
-export default async function SignInPage() {
-  return <Login />;
-}
