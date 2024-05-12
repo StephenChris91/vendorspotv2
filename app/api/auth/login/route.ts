c869f32935071c081811ba2b476f46cc12f098de
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-05-12 22:04:14 +0100

    extended user session to include extra information

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,49 +1,49 @@
-import { loginSchema } from "@/app/schemas";
-import { NextResponse } from "next/server";
-import { compareSync } from "bcrypt-ts";
-import { signIn } from "@/auth";
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
-import { getUserByEmail } from "@/lib/data/user";
-import { AuthError } from "next-auth";
+// import { loginSchema } from "@/app/schemas";
+// import { NextResponse } from "next/server";
+// import { compareSync } from "bcrypt-ts";
+// import { signIn } from "@/auth";
+// import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+// import { getUserByEmail } from "@/lib/data/user";
+// import { AuthError } from "next-auth";
 
-export async function POST(req: Request) {
-    const body = await req.json();
-    const validInputFields = loginSchema.safeParse(body)
+// export async function POST(req: Request) {
+//     const body = await req.json();
+//     const validInputFields = loginSchema.safeParse(body)
 
-    if (!validInputFields.success){
-        return {error: 'Your details are incorrect!'}
-    }
+//     if (!validInputFields.success){
+//         return {error: 'Your details are incorrect!'}
+//     }
 
-    const { email, password } = validInputFields.data;
+//     const { email, password } = validInputFields.data;
 
-    const existingUser = getUserByEmail(email)
+//     const existingUser = getUserByEmail(email)
 
-    if(!existingUser) {
-        return {error: 'This user does not exist!'}
-    }
+//     if(!existingUser) {
+//         return {error: 'This user does not exist!'}
+//     }
 
-    try {
-        await signIn('credentials', {
-            email,
-            password,
-            redirectTo: DEFAULT_LOGIN_REDIRECT
-        })
+//     try {
+//         await signIn('credentials', {
+//             email,
+//             password,
+//             redirectTo: DEFAULT_LOGIN_REDIRECT
+//         })
         
-    } catch (error) {
-        if(error instanceof AuthError) {
-            switch(error.type) {
-                case 'CredentialsSignin':
-                    return {error: 'Your details are incorrect!'}
-                default: return {error: 'Something Went Wrong!'}
-            }
-        }
+//     } catch (error) {
+//         if(error instanceof AuthError) {
+//             switch(error.type) {
+//                 case 'CredentialsSignin':
+//                     return {error: 'Your details are incorrect!'}
+//                 default: return {error: 'Something Went Wrong!'}
+//             }
+//         }
 
-        throw error
-    }
+//         throw error
+//     }
 
-    // return NextResponse.json({
-    //     status: "success",
-    //     message: "Login successful",
-    //     user: existingUser,
-    // });
-}
+//     // return NextResponse.json({
+//     //     status: "success",
+//     //     message: "Login successful",
+//     //     user: existingUser,
+//     // });
+// }
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,44 +1,44 @@
-import { NextResponse } from 'next/server';
-import { hash } from 'bcrypt-ts';
-import { db } from '@/prisma/prisma';
-import { signupSchema } from '@/app/schemas';
+// import { NextResponse } from 'next/server';
+// import { hash } from 'bcrypt-ts';
+// import { db } from '@/prisma/prisma';
+// import { signupSchema } from '@/app/schemas';
 
-export async function POST(req: Request) {
-    try {
-        const body = await req.json();
+// export async function POST(req: Request) {
+//     try {
+//         const body = await req.json();
 
-        const {email, password, firstname, lastname, role } = signupSchema.parse(body);
+//         const {email, password, firstname, lastname, role } = signupSchema.parse(body);
 
-        const existingUser = await db.user.findUnique({
-            where: {
-                email: email
-            }
-        })
-        if(existingUser) {
-            return NextResponse.json({user: null, message: 'A User with this email already exists'}, {status: 409})
-        }
+//         const existingUser = await db.user.findUnique({
+//             where: {
+//                 email: email
+//             }
+//         })
+//         if(existingUser) {
+//             return NextResponse.json({user: null, message: 'A User with this email already exists'}, {status: 409})
+//         }
 
-        const hashedPassword = await hash(password, 10)
+//         const hashedPassword = await hash(password, 10)
 
-        const newUser = await db.user.create({
-            data: {
-                email,
-                password: hashedPassword,
-                firstname,
-                lastname,
-                role
-            }
-        })
+//         const newUser = await db.user.create({
+//             data: {
+//                 email,
+//                 password: hashedPassword,
+//                 firstname,
+//                 lastname,
+//                 role
+//             }
+//         })
 
-        const { password: newUserPassword, ...rest} = newUser;
+//         const { password: newUserPassword, ...rest} = newUser;
 
-        return NextResponse.json({ message: `${role === 'Vendor' ? 'Signed Up Successfully! Welcome Vendor 😇' : 'Signed Up Successful! Happy Shopping'}`, user: rest}, {status: 201})
+//         return NextResponse.json({ message: `${role === 'Vendor' ? 'Signed Up Successfully! Welcome Vendor 😇' : 'Signed Up Successful! Happy Shopping'}`, user: rest}, {status: 201})
         
-        // return NextResponse.json(body)
-    } catch (error) {
-        console.log(error)
-    }
+//         // return NextResponse.json(body)
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-    return NextResponse.json({ message: 'User created successfully'})
+//     return NextResponse.json({ message: 'User created successfully'})
     
-}
+// }
