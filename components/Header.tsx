137295eba97d7683448fddc5272ccf70b4c232e9
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-05-12 23:19:01 +0100

    extended user session to include extra information

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,14 @@
-import Link from 'next/link';
-import bannerImg from '@/app/main-banner.png'
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import { Button } from './ui/button';
-import CategoryDropdown from './categories-dropdown';
-import UserComponent from './usercomponent';
+import Link from "next/link";
+import bannerImg from "@/app/main-banner.png";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import CategoryDropdown from "./categories-dropdown";
+// import UserComponent from './usercomponent';
 export default function Header() {
-
-
-
   return (
-    <div className="grid grid-flow-row grid-cols-5 gap-1 mx-auto items-center bg-gray-50 p-3 rounded-sm bg-cover bg-no-repeat bg-center before:content-['']
+    <div
+      className="grid grid-flow-row grid-cols-5 gap-1 mx-auto items-center bg-gray-50 p-3 rounded-sm bg-cover bg-no-repeat bg-center before:content-['']
     before:absolute
     before:inset-0
     before:block
@@ -18,12 +16,19 @@ export default function Header() {
     before:from-orange-200
     before:to-blue-500
     before:opacity-75
-    before:z-[-5]">
+    before:z-[-5]"
+    >
       <CategoryDropdown />
       <div className={cn("col-span-3 rounded-sm")}>
-        <Image src={bannerImg} alt="Main Banner" width={undefined} height={undefined} className='rounded-sm'/>
+        <Image
+          src={bannerImg}
+          alt="Main Banner"
+          width={undefined}
+          height={undefined}
+          className="rounded-sm"
+        />
       </div>
-      <UserComponent />
+      {/* <UserComponent /> */}
     </div>
   );
 }
