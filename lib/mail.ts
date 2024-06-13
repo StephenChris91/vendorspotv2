fa8ac7da8e7c1b1dfa9bd5ff906b4ae8c08b06e9
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-06-13 19:54:47 +0100

    modified email domain for production

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,34 +1,33 @@
-import { Resend } from 'resend'
+import { Resend } from 'resend';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_VERIFICATION_KEY)
+// Initialize the Resend instance
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_VERIFICATION_KEY);
 
-const domain = process.env.NEXT_PUBLIC_FRONTEND_DEV_URL
+// Determine the domain based on the environment
+const domain = process.env.NODE_ENV === 'production'
+  ? process.env.NEXT_PUBLIC_FRONTEND_PROD_URL
+  : process.env.NEXT_PUBLIC_FRONTEND_DEV_URL;
 
-export const sendVerificationEmail = async (
-    email: string,
-    token: string
-) => {
-    const confirmationLink = `${domain}/auth/new-verification?token=${token}`;
+// Function to send verification email
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmationLink = `${domain}/auth/new-verification?token=${token}`;
 
-    await resend.emails.send({
-        from: 'Vendorspot Notification <admin@vendorspot.ng>',
-        to: email,
-        subject: 'Verify your email address',
-        html: `<p>Please click the link below to verify your email address: <a href="${confirmationLink}">${confirmationLink}</a></p>`
-    })
-}
+  await resend.emails.send({
+    from: 'Vendorspot Notification <admin@vendorspot.ng>',
+    to: email,
+    subject: 'Verify your email address',
+    html: `<p>Please click the link below to verify your email address: <a href="${confirmationLink}">${confirmationLink}</a></p>`
+  });
+};
 
+// Function to send reset password email
+export const sendResetPasswordEmail = async (email: string, token: string) => {
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-export const sendResetPasswordEmail = async (
-    email: string,
-    token: string
-) => {
-    const resetLink = `${domain}/auth/new-password?token=${token}`;
-
-    await resend.emails.send({
-        from: 'admin@vendorspot.ng',
-        to: email,
-        subject: 'Reset Password',
-        html: `<p>Please click : <a href="${resetLink}">here</a> below to verify your email address> </p>`
-    })
-}
+  await resend.emails.send({
+    from: 'admin@vendorspot.ng',
+    to: email,
+    subject: 'Reset Password',
+    html: `<p>Please click : <a href="${resetLink}">here</a> below to verify your email address> </p>`
+  });
+};
